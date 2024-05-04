d86134bf526c93e39d47e54b0cf088937596d80d
paodelonga <72471292+paodelonga@users.noreply.github.com>
2024-05-03 23:03:12 -0300

    fish: add enviroment vars for user custom-paths

--- etc/fish/conf.d/enviroment.fish
+++ etc/fish/conf.d/enviroment.fish
@@ -4,8 +4,8 @@ export EDITOR="micro"
 export IDE="nvim"
 export PAGER="cat"
 
-export REPOS_PATH=""
-export DOTFILES_PATH="$REPOS_PATH/dot-denver"
+export REPOS_PATH="/root/repositories/"
+export DOTFILES_PATH="/root/repositories/omv-dotfiles"
 
 # Set GPG_TTY to current terminal
 export GPG_TTY=(tty)
