936d5b984b9ec48510e631749277314641f750c8
paodelonga <72471292+paodelonga@users.noreply.github.com>
2024-06-07 15:12:24 -0300

    bash: add bash enviroment profile

---
+++ etc/bash.bashrc_custom/enviroment/profile.sh
@@ -0,0 +1,12 @@
+# enviroment variables
+export REPOS_PATH="/home/paodelonga/repositories/"
+export BROWSER="firefox"
+export EDITOR="micro"
+export IDE="nvim"
+export PAGER="cat"
+
+# configure pinentry to current tty
+export GPG_TTY=(tty)
+
+# add directories to $PATH
+# PATH="${PATH:+${PATH}:}/sbin/"
